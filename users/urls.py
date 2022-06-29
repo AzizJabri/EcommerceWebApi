cc3a13be2c33f5a11bbd06274b47bf6103437665
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2022-06-29 11:43:13 +0100

    :sparkles: created api route for users.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from knox import views as knox_views
-from .views import LoginAPI, ChangePasswordView, RegisterAPI
+from .views import LoginAPI, ChangePasswordView, RegisterAPI, UsersListAPI, UserDetailAPI
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
@@ -10,4 +10,6 @@ urlpatterns = [
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('password_reset/', include('django_rest_passwordreset.urls',
          namespace='password_reset')),
+    path('users/', UsersListAPI.as_view(), name='users'),
+    path('user/<int:pk>/', UserDetailAPI.as_view(), name='user-detail'),
 ]
